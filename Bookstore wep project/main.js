d99d28790e6641e37a0d9a7acc1d98f31d970b55
kalgetachew375@gmail.com <kalgetachew375@gmail.com>
2023-08-01 11:28:17 +0300

    deleting existing design

--- Bookstore wep project/main.js
+++
@@ -1,5 +0,0 @@
-document.querySelector(".drawer").addEventListener("click", function () {
-  let draw = document.querySelector(".slide");
-  draw.style.display = "flex";
-  draw.style.position = "static";
-});
