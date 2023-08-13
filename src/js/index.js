88accd039acddf29ca20fa7768401ba18a2bf591
kalgetachew375@gmail.com <kalgetachew375@gmail.com>
2023-08-13 20:27:47 +0300

    adding some slides

--- src/js/index.js
+++ src/js/index.js
@@ -54,7 +54,37 @@
     });
 
   });
+
+  $(document).ready(function() {
+
+    $("#top-rated").owlCarousel({
+      items: 8,
+      nav: false,
+      loop: true,
+      margin: 10,
+      dots: false,
+      autoplay: true,
+      slideTransition: 'linear',
+      autoplayTimeout: 6000,
+      autoplaySpeed: 8000,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          items: 2
+        },
+        600: {
+          items: 3
+        },
+        1000: {
+          items: 5
+        }
+      }
+
+    });
+
+  });
   const container = document.querySelector('#bestseller');
+  const topRated = document.querySelector('#top-rated')
   for (var i = 0; i < 12; i++) {
     const chaild = `
            <div class="card shadow my-4">
@@ -72,5 +102,23 @@
             </div>
                         `;
 
+
+    const chaildT = `
+           <div class="card shadow my-4">
+              <img src="./src/Image/pro_pbid_4217056_380x.jpeg" alt="" class="card-img-top">
+              <div class="card-body pb-5">
+                <p class="card-text m-0  category">
+                  <small>${i} item</small>
+                </p>
+                <h4 class="card-title my-2 text-capitalize title">Discover the art</h4>
+                <h5 class="card-text m-0 mb-2">
+                  <small>chaos</small>
+                </h5>
+                <a href="" class="btn btn-lg btn-outline-primary price">$400</a>
+              </div>
+            </div>
+                        `;
+
     container.innerHTML += chaild;
+    topRated.innerHTML += chaildT;
   }
