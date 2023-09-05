42b63f7fb5728554286581dfa22b17d1973d6a30
kalgetachew375@gmail.com <kalgetachew375@gmail.com>
2023-09-05 20:26:42 +0300

    update book card

--- src/js/index.js
+++ src/js/index.js
@@ -177,19 +177,39 @@
   const topRated = document.querySelector('#top-rated');
   for (var i = 0; i < 12; i++) {
     const chaild = `
-           <div class="card shadow my-4">
-              <img src="${image[Math.round((Math.random()*3))]}" alt="" class="card-img-top">
-              <div class="card-body pb-5">
-                <p class="card-text m-0  category">
-                  <small>${i} item</small>
-                </p>
-                <h4 class="card-title my-2 text-capitalize title">Discover the art</h4>
-                <h5 class="card-text m-0 mb-2">
-                  <small>chaos</small>
-                </h5>
-                <a href="" class="btn btn-lg btn-outline-primary price">$400</a>
-              </div>
-            </div>
+  <div class="card shadow my-4 book-card" id="book-card">
+    <div class="position-relative">
+      <div class="overlay rounded-top">
+        <div class="social-media">
+          <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+          <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+          <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        </div>
+      </div>
+   <img src="${image[Math.floor((Math.random()*3))]}" alt="" />
+    </div>
+    <div class="card-body">
+      <h5 class="card-title ">{value.book_title}</h5>
+      <p class="card-text">{value.book_author}</p>
+      <p class="card-text">$<span class="text-dange">300</span> <del class="text-muted"></del></p>
+      <div class="d-flex justify-content-between align-items-center">
+        <a href="#" class="btn btn-success">Add to Cart</a>
+        <a href="#" class="btn btn-outline-secondary"><i class="bi bi-heart"></i></a>
+      </div>
+      <div class="rating">
+        <input type="radio" name="rating" id="rating-5">
+        <label for="rating-5"><i class="bi bi-star-fill"></i></label>
+        <input type="radio" name="rating" id="rating-4">
+        <label for="rating-4"><i class="bi bi-star-fill"></i></label>
+        <input type="radio" name="rating" id="rating-3">
+        <label for="rating-3"><i class="bi bi-star-fill"></i></label>
+        <input type="radio" name="rating" id="rating-2">
+        <label for="rating-2"><i class="bi bi-star-fill"></i></label>
+        <input type="radio" name="rating" id="rating-1">
+        <label for="rating-1"><i class="bi bi-star-fill"></i></label>
+      </div>
+    </div>
+  </div> 
                         `;
 
 
